abc5972505fdb19b2eb299f1ee9127a5c0f78096
Jason <jasonmili.ece@gmail.com>
2017-02-23 21:06:00 +0200

    Initial reading of data

---
+++ round/sol.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+using namespace std;
+
+int v, e, r, c, max_cap, video_size[10005];
+
+int main() {
+	
+	int i;
+	
+	scanf("%d%d%d%d%d", &v, &e, &r, &c, &max_cap);
+	for (i=0; i < v; i++)
+		scanf("%d", &video_size[i]);
+	
+	
+
+}
